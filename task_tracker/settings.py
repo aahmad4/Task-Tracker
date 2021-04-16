35a188218099557921b6469bcbeef163e60fb282
Ali Ahmad <alislearning123@gmail.com>
2021-04-16 21:50:00 +0000

    Remove Old Cached Changes

--- task_tracker/settings.py
+++ task_tracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '3@!^y8ms$36cub%(vqyc8gopzb^sivzq4zxn2l&fdniggx64n&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aahmad2.pythonanywhere.com']
 
 
 # Application definition
@@ -119,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGIN_URL = '/login'
